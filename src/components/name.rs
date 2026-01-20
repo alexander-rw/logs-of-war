4924a36bed234d4bba67f4c4d27f2b853f22db85
Alexander Reyes-Wainwright <alexrw17@gmail.com>
2026-01-20 17:46:38 +0000

    hello world (with some spam)

---
+++ src/components/name.rs
@@ -0,0 +1,5 @@
+use bevy::prelude::Component;
+
+#[allow(dead_code)]
+#[derive(Component)]
+pub struct Name(pub String);
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use bevy::prelude::*;
+
+mod components;
+
+use crate::components::person::Person;
+use crate::components::name::Name;
+
+fn hello_world_system() {
+    println!("hello world");
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, add_people)
+        .add_systems(Update, hello_world_system).run();
+}
+
+pub fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
